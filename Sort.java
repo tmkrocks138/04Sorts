cb35061c769d455cea35039e4f3b0e48a8602dc5
tmkrocks138 <tkhanna@stuy.edu>
2016-12-13 06:30:34 -0500

    Skeleton of the code...literally nothing

---
+++ Sort.java
@@ -0,0 +1,16 @@
+public class Sort{
+
+  /**EDIT THIS METHOD TO MATCH YOUR NAME 
+  */
+  public static String name(){
+    return "01.Khanna.Tara"; 
+  }
+
+  /**Selection sort of an int array. 
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
+  public static void selectionSort(int[] data){  }
+
+
+}
