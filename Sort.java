983deb5396d67ec1d7659269c5d710c524fe429b
Tara Khanna <tmkrocks138@Taras-MacBook-Air.local>
2016-12-15 21:36:06 -0500

    Bubble added: my code SORT-of works

--- Sort.java
+++ Sort.java
@@ -22,10 +22,10 @@ public class Sort{
 	  int smallest = data[i];
 	  int oldSpot = i;
 	  int newSpot = i;
-	  
+	  int temp = data[i];	  
 	  //simulates going through the unsorted stuff
 	  //looks for smallest in the unsorted stuff
-	  for (int j = i = 1; j < data.length; j++){
+	  for (int j = i + 1; j < data.length; j++){
 	      if (data[j]<smallest){
 		  smallest = data[j];
 		  newSpot = j;
@@ -33,7 +33,7 @@ public class Sort{
 	  }
 	  
 	  //Switch smallest with in smallest spot
-	  int temp = data[i];
+	  temp = data[i];
 	  data[oldSpot]=smallest;
 	  data[newSpot]=temp;
 	  
@@ -43,22 +43,38 @@ public class Sort{
   }
   public static void insertion(int[] data){
 //length of sorted	
-      for(int i = 1; i < data.length; i++){
-	int temp=data[i];
-	int num = i-1;
-	while(temp<data[num]){
-	    num--;
-	}
-	for (int j = i - 1; j > num; j--){
-	    data[j+1]=data[j];
-	}
-	data[num]=temp;
-    }
+    for(int i = 0; i < data.length-1; i++){
+	    int num = i + 1;
+	    int temp=data[num];
+	    int smallest=i;
+	    for (int j = i; j >= 0; j--){
+		if (temp < data[j]){
+		    smallest = j;
+		    data[j+1]=data[j];
+		}
+	    }
+	    data[smallest]=temp;
+}
   }
   public static void bubble(int[] data){
 //nested for loop
 //count switch numbers and if switch is 0 at end, end loop early
-
+	boolean done = false;
+	for (int times = 0; times < data.length && !done ; times++){
+	    int swaps = 0;
+	    for (int current = 0; current<data.length-times-1; current++){
+		int nextTo=current+1;
+		int tempC = data[current];
+		if (tempC>data[nextTo]){
+		    data[current]=data[nextTo];
+		    data[nextTo]=tempC;
+		    swaps++;
+		}
+	    }
+	    if (swaps == 0){
+		done = true;
+	    }
+	}
 }
 
 
